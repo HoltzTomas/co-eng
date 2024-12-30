4b5411571977e4bb554d20f6418d32c5f6e77a43
Julian Guerrero <julian1guerrero@gmail.com>
2024-12-30 17:10:06 -0300

    change ask-documents prompts

--- app/api/ask-documents/route.ts
+++ app/api/ask-documents/route.ts
@@ -1,9 +1,14 @@
 import { cosineSimilarity, embed, generateText, Message, streamText } from 'ai'
-import { providers, ProviderName } from '@/lib/ai-providers'
+import { providers } from '@/lib/ai-providers'
 import { google } from '@ai-sdk/google'
+
+import { db } from '@/lib/db/db'
 import { getChunksByFileId } from '@/lib/db/queries'
+import { inArray } from 'drizzle-orm'
+import { files as filesTable } from '@/lib/db/schema'
 
 type ChunkSimilarity = {
+  file: string
   text: string
   similarity: number
 }
@@ -17,15 +22,15 @@ export async function POST(req: Request) {
     maxTokens: 512,
     temperature: 0.3,
     maxRetries: 1,
-    system: "Answer this question with a short paragraph"
+    system: "Answer this question with a short paragraph."
   })
-
-  console.log("Temp response: " + tempResponse.text)
 
   const tempEmbedding = await embed({
     model: google.textEmbeddingModel("text-embedding-004"),
     value: tempResponse.text
   })
+
+  const files = await db.select().from(filesTable).where(inArray(filesTable.id, filesid))
 
   let similarityList: ChunkSimilarity[] = []
   for (const fileid of filesid) {
@@ -36,6 +41,7 @@ export async function POST(req: Request) {
         value: chunk.content
       })
       similarityList.push({
+        file: files.find(file => file.id === fileid)!.name,
         text: chunk.content,
         similarity: cosineSimilarity(result.embedding, tempEmbedding.embedding)
       })
@@ -45,33 +51,42 @@ export async function POST(req: Request) {
   similarityList.sort((a, b) => b.similarity - a.similarity)
   similarityList = similarityList.slice(0, 10)
 
-  let prompt: string = "Here are the most relevant paragraphs from the documents you uploaded:\n"
+  let context: string = "";
 
   similarityList.forEach((chunk, index) => {
-    prompt += `${index + 1}. ${chunk.text}\n\n`
+    context += `${index + 1}. File: ${chunk.file}\n${chunk.text}\n\n`
   })
 
-  let message = messages.pop()
-  prompt += `With this paragraphs, answer the following question: ${message!.content}\n`
+  const message = messages.pop()
 
-  console.log("Final prompt: " + prompt)
+  const prompt = `Use the following pieces of context to answer the question at the end.
+Answer in the same language as the question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+Keep the answer concise. Mention the name of the document that you used to answer the question.
+
+${context}
+
+
+Question: ${message!.content}
+
+Helpful Answer:`
 
   messages.push({
     id: message!.id,
     content: prompt,
     role: "user"
   })
-  
+
   const result = streamText({
-      model: providers.gemini,
-      messages,
-      maxTokens: 512,
-      temperature: 0.3,
-      maxRetries: 1,
+    model: providers.gemini,
+    messages,
+    maxTokens: 512,
+    temperature: 0.3,
+    maxRetries: 1,
   })
-  
+
   return result.toDataStreamResponse({
-    getErrorMessage: (e: unknown) => {
+    getErrorMessage: (_e) => {
       return "Something went wrong"
     }
   })
