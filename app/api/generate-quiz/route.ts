203f1cb117d5c673bd41da16958293397ddd756d
Facundo López Llopis <flopezllopis@itba.edu.ar>
2024-12-30 21:32:22 -0300

    Initial quiz feature Quiz generation logic and UI

---
+++ app/api/generate-quiz/route.ts
@@ -0,0 +1,64 @@
+import { getChunksByFileId, getRandomChunksByFileId } from "@/lib/db/queries";
+import { questionSchema, questionsSchema } from "@/lib/schemas/quiz";
+import { google } from "@ai-sdk/google";
+import { CoreMessage, streamObject } from "ai";
+
+export const maxDuration = 60;
+
+export async function POST(req: Request) {
+  const { filesid, numQuestions }: { filesid: number[], numQuestions: number } = await req.json();
+
+  if (filesid.length === 0) return new Response("No files selected", { status: 400 });
+  if (numQuestions < 1 || numQuestions > 20) return new Response("Invalid number of questions", { status: 400 });
+  
+  let chunks = [];
+  for (const id of filesid) {
+    const fileChunks = await getRandomChunksByFileId(id, 10);
+    chunks.push(fileChunks.map((chunk) => chunk.content).join(" - CHUNK CHANGE - "));
+  }
+
+  console.log(chunks);
+
+  const documents: CoreMessage[] = chunks.map((chunk) => ({ 
+    role: "system", 
+    content: "File: " + chunk 
+  }));
+
+  
+  
+
+  const result = streamObject({
+    model: google("gemini-1.5-pro-latest"),
+    messages: [
+      {
+        role: "system",
+        content:
+          `You are a teacher. Your job is to take a document, and create a multiple choice test (with ${numQuestions} questions) based on the content of the document. 
+          Only one of the options should be correct.
+          The questions have to be strictly about the main content of the document, no about subjects related to it.
+          Each option should be roughly equal in length.
+          Answer in the same language as the document.`,
+      },
+      ...documents,
+      {
+        role: "user",
+        content: [
+          {
+            type: "text",
+            text: "Create a multiple choice test based on this documents.",
+          }
+        ],
+      },
+    ],
+    schema: questionSchema,
+    output: "array",
+    onFinish: ({ object }) => {
+      const res = questionsSchema.safeParse(object);
+      if (res.error) {
+        throw new Error("Something went wrong");
+      }
+    },
+  });
+
+  return result.toTextStreamResponse();
+}
---
+++ components/quiz.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { useState } from "react"
+import { File } from "@/lib/db/types";
+import { Question, questionsSchema } from "@/lib/schemas/quiz";
+import { experimental_useObject } from "ai/react";
+import { toast } from "@/hooks/use-toast";
+import QuizOptions from "./quiz-options";
+import QuizPlayer from "./quiz-player";
+import { Card } from "./ui/card";
+import { Progress } from "./ui/progress";
+import { motion } from "framer-motion";
+
+export default function Quiz({ files }: { files: File[] }) {
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [numQuestions, setNumQuestions] = useState(5);
+  const { submit, object: partialQuestions, isLoading } = experimental_useObject({
+      api: "/api/generate-quiz",
+      schema: questionsSchema,
+      initialValue: undefined,
+      onError: (error) => {
+          toast({
+              title: "Error",
+              description: "No se pudo generar el quiz. Por favor, intente de nuevo.",
+              variant: "destructive",
+          })
+      },
+      onFinish: ({ object }) => {
+          if (object && object.length > 0) {
+              setQuestions(object);
+              console.log(object);
+          }
+      },
+  });
+
+  if (questions.length === 0) {
+    const progress = partialQuestions ? (partialQuestions.length / numQuestions) * 100 : 0;
+
+    return (
+      <div className="w-full max-w-md mx-auto space-y-4">
+        <QuizOptions files={files} onSubmit={(data) => {
+          submit(data);
+          setNumQuestions(data.numQuestions);
+        }} />
+        {isLoading && (
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 20 }}
+            transition={{ duration: 0.2 }}
+          >
+            <Card className="flex flex-col space-y-2 p-6">
+              <div className="w-full space-y-1">
+                <div className="flex justify-between text-sm text-muted-foreground">
+                  <span>Progress</span>
+                  <span>{Math.round(progress)}%</span>
+                </div>
+                <Progress value={progress} className="h-2" />
+              </div>
+              <div className="w-full space-y-2">
+                <div className="items-center space-x-2 text-sm">
+                  <span className="text-muted-foreground">
+                    {partialQuestions
+                      ? `Generating question ${partialQuestions.length + 1} of ${numQuestions}`
+                      : "Analyzing PDF content"}
+                  </span>
+                </div>
+              </div>
+            </Card>
+          </motion.div>
+        )}
+      </div>
+    )
+  }
+  else {
+    return (
+      <QuizPlayer questions={questions} onReset={() => {setQuestions([])}} />
+    )
+  }
+}
