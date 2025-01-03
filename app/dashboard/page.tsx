13b60caa70a6a9e43ab6c3637c7599af87cae3da
Julian Guerrero <julian1guerrero@gmail.com>
2025-01-03 16:55:20 -0300

    blank dashboard message

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@
 export default async function Page() {
   return (
     <main className="flex-1 mx-auto my-auto">
-      <p className="text-center">Seleccione una materia o cree una para comenzar</p>
+      <p className="text-center">Seleccione una carpeta o cree una para comenzar</p>
     </main>
   )
 }
