ccb8c26b7deb9cfd9217f8ae890c3190e4ec9f4c
Tomas Holtz <tomiholtz@gmail.com>
2025-01-03 17:59:46 -0300

    Revert "Adding Analytics"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import { ClerkProvider } from '@clerk/nextjs';
-import { Analytics } from "@vercel/analytics/react"
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { Metadata } from 'next/types';
@@ -18,7 +17,6 @@ export default function RootLayout({ children }: React.PropsWithChildren) {
       <ClerkProvider>
         <body className={`${inter.className} bg-gradient-to-br from-gray-100 to-gray-200`}>
           {children}
-          <Analytics/>
         </body>
       </ClerkProvider>
     </html>
